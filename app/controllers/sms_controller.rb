9b3c355a35c60a540186fe2e8d7aced63cd5a6c4
Greg Baugues <greg@baugues.com>
2014-02-11 11:41:01 -0800

    add sms spec

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -1,2 +1,13 @@
-class SmsController < ApplicationController
+class SMSController < ApplicationController
+
+  def create
+    user = User.find_by (phone_number: params[:From])
+    if user
+
+    else
+      User.create(params[params[:From], params[:Body])
+    end
+
+  end
+
 end
