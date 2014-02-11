94f4c7b66668170248775fc7c689a335e9cca5cb
Greg Baugues <greg@baugues.com>
2014-02-10 22:16:55 -0800

    First commit

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.2'
+gem 'postgres'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'turbolinks'
+gem 'haml'
+gem 'rails_config'
+
+group :testing do
+  gem 'pry'
+end
+
+group :doc do
+  # bundle exec rake doc:rails generates the API under doc/api.
+  gem 'sdoc', require: false
+end
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Moodilio::Application.config.secret_key_base = 'c9d1edb88480828a794e29b20e877dc27c865c19418a4e3c46ff48354a13ffb3bf66453f08e6dd880ca2bf7faa8e4bd4683a863858c754eb573164a6250a000f'
