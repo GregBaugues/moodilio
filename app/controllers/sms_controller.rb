922a5fb6872e01de9491e3f0f72ac54838261db5
Greg Baugues <greg@baugues.com>
2014-02-12 15:45:49 -0800

    refactor controller

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -3,20 +3,26 @@ class SmsController < ApplicationController
   def incoming
     user = User.find_by(phone_number: params['From'])
     if user.present?
-      score = params['Body'].strip.chars.first.to_i
-      if (1..5).include?(score)
-        if user.create_mood(score)
-          user.mood_message(score)
-        else
-          user.text_error
-        end
-      else
-        user.text_error
-      end
+      check_input(user, params)
     else
-      @user = User.create(phone_number: params['From'], name: params['Body'])
-      @user.confirm_signup
-      render :empty_response, layout: false
+      new_user(params)
+    end
+    render :empty_response, layout: false
+  end
+
+  def new_user(params)
+    user = User.create(phone_number: params['From'], name: params['Body'])
+    user.confirm_signup
+  end
+
+  def check_input(user, params)
+    body = params['Body'].strip.chars.first.to_i
+    if (1..5).include?(body)
+      score = body
+      user.create_mood(score)
+      user.mood_message(score)
+    else
+      user.text_error
     end
   end
 
