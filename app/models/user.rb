8f2074ea44ec436d0fb0ccb87aaeab6a714660e7
Greg Baugues <greg@baugues.com>
2014-02-12 12:45:49 -0800

    add backfill moods and update user index

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_many :moods, -> { order("date DESC") }
   after_create :confirm_signup
+  after_create :backfill_moods
 
   def confirm_signup
     client = Twilio::REST::Client.new Settings.account_sid, Settings.auth_token
@@ -13,6 +14,17 @@ class User < ActiveRecord::Base
 
   def create_mood(date, score)
     Mood.create(user_id: id, date: date, score: score)
+  end
+
+  def backfill_moods
+    modifier = [-1, 0, 1].sample
+    (0..6).collect do |i|
+      date = Date.today - i.days
+      score = (1..5).to_a.sample + modifier
+      score += 1 if score == 0
+      score -= 1 if score == 6
+      create_mood(date, score)
+    end
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,17 @@
 describe 'User' do
 
   before (:each) do
-    User.create(name: "Bob", phone_number: "+13125551234")
+    @user = User.create(name: "Bob", phone_number: "+13125551234")
   end
 
   it 'should create a mood', :vcr do
-    user = User.first
-    user.create_mood(Date.today, 5)
-    user.moods.first.score.should == 5
+    @user.create_mood(Date.today, 5)
+    @user.moods.first.score.should == 5
+  end
+
+  it 'should backfill modds', :vcr do
+    @user.backfill_moods
+    @user.moods.size.should == 7
   end
 
 end
